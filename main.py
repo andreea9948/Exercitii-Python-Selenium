3c9753ba13da6e90fd30d52a5d1f99c3c052a87d
Cojocaru <ancojocaru@luxoft.com>
2022-07-15 13:01:35 +0300

    added messages

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import time
 def open_close(func):
     def wrapper():
         page = HomePage()
-        func(page)
+        msg = func(page)
+        print(msg)
         time.sleep(10)
         Driver.close_driver()
     return wrapper
@@ -21,7 +22,9 @@ def run_ex1(page):
     page.scroll_until_xpath('.//div[@class="row download-list-widget"]')
     latest_release = TableReleases.get_number(page.table_releases.get_elem_by_index(1))
 
-    print(f'Cel mai nou release - {latest_release}')
+    message = f'Cel mai nou release - {latest_release}'
+
+    return message
 
 
 @open_close
@@ -35,12 +38,20 @@ def run_ex2(page):
     examples = PepPage.find_in_contents('Examples')
     PepPage.click_element(examples)
     nr_examples = PepPage.count_bullets('.//section[@id="examples"]/ol/li')
-    PepPage.check_nr_bullets(5, nr_examples)
+
+    message = ''
+    if nr_examples == 5:
+        message = f'Corect - Sunt 5 exemple'
+    else:
+        message = f'Gresit! Numarul de exemple gasit este {nr_examples}'
+
+    return message
 
 
 @open_close
 def run_ex3(page):
     #ex3
+    message = ''
     submenu_downloads = page.navMenu.hover_category("downloads")
     all_rls = page.navMenu.search_subcategory("All releases", submenu_downloads)
     HomePage.click_element(all_rls)
@@ -50,9 +61,9 @@ def run_ex3(page):
 
     count_rls = page.table_releases.get_releases_by_version(version)
     if count_rls is not None:
-        print(f'Exista {len(count_rls)} release-uri pentru ultima versiune')
+        message = f'Exista {len(count_rls)} release-uri pentru ultima versiune\n'
     else:
-        print('Nu exista release pentru ultima versiune')
+        messgae = 'Nu exista release pentru ultima versiune\n'
 
     latest_release = page.table_releases.get_elem_by_index(1)
     latest_release_date = TableReleases.get_date(latest_release)
@@ -62,12 +73,13 @@ def run_ex3(page):
     first_release_date = TableVersions.get_date(first_release)
 
     if first_release_date < latest_release_date:
-        print(f'Gresit - data primului release este {first_release_date} si a ultimului {latest_release_date}')
+        message += f'Gresit - data primului release este {first_release_date} si a ultimului {latest_release_date}'
     else:
-        print(f'Corect - data primului release este {first_release_date} si a ultimului {latest_release_date}')
+        message += f'Corect - data primului release este {first_release_date} si a ultimului {latest_release_date}'
 
+    return message
 
 if __name__ == "__main__":
-    #run_ex1()
+    run_ex1()
     #run_ex2()
-    run_ex3()
+    #run_ex3()
