4ebf0a7a4fb74ac320ef82428672076e14523dfc
Cojocaru <ancojocaru@luxoft.com>
2022-07-15 09:36:25 +0300

    move first scripts to first_try folder

---
+++ first_try/main.py
@@ -0,0 +1,8 @@
+from ex1navig import *
+from ex2dec import *
+from ex3table import *
+
+if __name__ == "__main__":
+    ex1()
+    #ex2()
+    #ex3()
--- first_try/open_close.py
+++ first_try/open_close.py
@@ -5,7 +5,7 @@ import time
 def open_close(func):
     def wrapper():
         # Create driver and open python.org
-        driver = webdriver.Chrome("./chromedriver_win32/chromedriver.exe")
+        driver = webdriver.Chrome("../chromedriver_win32/chromedriver.exe")
         driver.get("https://www.python.org")
         message = func(driver)
         print(message)
